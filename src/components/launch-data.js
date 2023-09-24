095a3650f85efa23f7f4410f842027eabc519156
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-09-24 21:54:25 +0300

    query upcoming

--- src/components/launch-data.js
+++ src/components/launch-data.js
@@ -1,15 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import ReactSelect from "react-select";
 
 const SpaceXLaunches = () => {
   const [launches, setLaunches] = useState([]);
   const [filter, setFilter] = useState("latest");
+  const [search, setSearch] = useState("");
+
+  // Check the type of the launches variable before calling the filter() method.
+  const filteredLaunches =
+    launches && Array.isArray(launches)
+      ? launches.filter((launch) => {
+          return launch.name.includes(search) && launch.status === filter;
+        })
+      : [];
 
   useEffect(() => {
     const fetchLaunches = async () => {
-      let url = "https://api.spacexdata.com/v5/launches/latest";
-      if (filter === "upcoming") {
-        url = "https://api.spacexdata.com/v5/launches/upcoming";
+      let url = "https://api.spacexdata.com/v5/launches";
+      if (filter === "latest") {
+        url += "/latest";
+      } else if (filter === "upcoming") {
+        url += "/upcoming";
+      } else if (filter === "success") {
+        url += "?filter=status:success";
+      } else if (filter === "failed") {
+        url += "?filter=status:failed";
       }
 
       const response = await axios.get(url);
@@ -21,26 +37,46 @@ const SpaceXLaunches = () => {
   }, [filter]);
 
   const handleFilterChange = (event) => {
-    setFilter(event.target.value);
+    setFilter(event.value);
+  };
+
+  const handleInputChange = (event) => {
+    setSearch(event.target.value);
   };
 
   return (
     <div>
       <h1>SpaceX Launches</h1>
-      <select value={filter} onChange={handleFilterChange}>
-        <option value="latest">Latest</option>
-        <option value="upcoming">Upcoming</option>
-      </select>
+      <input
+        type="text"
+        placeholder="Search launches..."
+        value={search}
+        onChange={handleInputChange}
+      />
+      <ReactSelect
+        options={[
+          { value: "latest", label: "Latest" },
+          { value: "upcoming", label: "Upcoming" },
+          { value: "success", label: "Success" },
+          { value: "failed", label: "Failed" },
+        ]}
+        onChange={handleFilterChange}
+        value={filter}
+      />
       <ul>
-        {Array.isArray(launches) &&
-          launches.map((launch) => (
+        {/* Only render the list of launches if the launches variable is an array. */}
+        {launches && Array.isArray(launches) ? (
+          filteredLaunches.map((launch) => (
             <li key={launch.id}>
               <h3>{launch.name}</h3>
               <p>Launch Date: {launch.date_local}</p>
               <p>Rocket: {launch.rocket.name}</p>
               <p>Mission: {launch.details}</p>
             </li>
-          ))}
+          ))
+        ) : (
+          <p>No launches found.</p>
+        )}
       </ul>
     </div>
   );
